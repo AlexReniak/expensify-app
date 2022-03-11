7fb35523ac8e2a24519dc9191d735e935f3c8ef5
Alex R <areniak147@hotmail.com>
2022-03-11 12:35:22 -0500

    remove dummy data

--- src/app.js
+++ src/app.js
@@ -13,18 +13,6 @@ import 'react-dates/initialize'
 
 const store = configureStore()
 
-store.subscribe(() => {
-    console.log(store.getState())
-})
-
-store.dispatch(addExpense({ description: 'water bill', amount: 500, createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'gas bill', amount: 400 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 1000 }))
-
-const state = store.getState()
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-// console.log(visibleExpenses)
-
 const Jsx = () => (
     <Provider store={store}>
         <AppRouter />
